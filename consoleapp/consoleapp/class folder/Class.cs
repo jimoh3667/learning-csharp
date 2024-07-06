a8659ddab41c8c9fd472f9f33bdda8b01d1c22f6
jimoh3667 <jimohridwan19@gmail.com>
2024-07-06 11:32:44 +0100

    Add project files.

---
+++ consoleapp/consoleapp/class folder/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp.class_folder
+{
+    internal class Class
+    {
+        private static void MyMethod()
+
+        Console.Writeline("i just got here");
+
+    }
+
+    private static void Main(string[] args)
+     private MyMethod();
+}
